b7d419799f8c00b06b311c6c78fc276ecbc3993e
開発 拓也 <tkaihatsu@ecbeing.co.jp>
2020-06-25 09:30:18 +0900

    日付枠を追加

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,45 @@
         class B division.
       </div>
 
+<div class="day">
+  <span>8:00</span><div class="hour"></div>
+  <span>9:00</span><div class="hour"></div>
+  <span>10:00</span><div class="hour"></div>
+  <span>11:00</span><div class="hour"></div>
+  <span>12:00</span><div class="hour"></div>
+  <span>13:00</span><div class="hour"></div>
+  <span>14:00</span><div class="hour"></div>
+  <span>15:00</span><div class="hour"></div>
+  <span>16:00</span><div class="hour"></div>
+  <span>17:00</span><div class="hour"></div>
+  <span>18:00</span><div class="hour"></div>
+</div>
+<div class="day">
+  <span>8:00</span><div class="hour"></div>
+  <span>9:00</span><div class="hour"></div>
+  <span>10:00</span><div class="hour"></div>
+  <span>11:00</span><div class="hour"></div>
+  <span>12:00</span><div class="hour"></div>
+  <span>13:00</span><div class="hour"></div>
+  <span>14:00</span><div class="hour"></div>
+  <span>15:00</span><div class="hour"></div>
+  <span>16:00</span><div class="hour"></div>
+  <span>17:00</span><div class="hour"></div>
+  <span>18:00</span><div class="hour"></div>
+</div>
+<div class="day">
+  <span>8:00</span><div class="hour"></div>
+  <span>9:00</span><div class="hour"></div>
+  <span>10:00</span><div class="hour"></div>
+  <span>11:00</span><div class="hour"></div>
+  <span>12:00</span><div class="hour"></div>
+  <span>13:00</span><div class="hour"></div>
+  <span>14:00</span><div class="hour"></div>
+  <span>15:00</span><div class="hour"></div>
+  <span>16:00</span><div class="hour"></div>
+  <span>17:00</span><div class="hour"></div>
+  <span>18:00</span><div class="hour"></div>
+</div>
     </section>
   </div>
 </template>
@@ -39,4 +78,17 @@ section{
 .B{
   background-color: lightcoral;
 }
-</style>>
+
+.day{
+  display: inline;
+}
+
+.hour{
+  margin: 0;
+  border: 1px solid black;
+}
+
+span{
+  vertical-align: top;
+}
+</style>
